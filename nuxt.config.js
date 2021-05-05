03db24753d82e861313fcf689e36e68aa6c02017
linre-90 <juho.lindemark@gmail.com>
2021-05-05 18:09:31 +0300

    Updated sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -101,6 +101,7 @@ export default {
         "/Z2KA0Bb7PCLQeTJ3EU0I/en/blogreader",
         "/jjOoYHSwb0KVo49Tnvlo/en/blogreader",
         "/wpGTN4tY9GOthlJuf6wU/en/blogreader",
+        "/ZrzfwrC0GUhk0EFTbJFu/en/blogreader",
         /*FI */
         "/fi/5LeiwDXK8907D5uk1vhx/fi/blogreader",
         "/fi/g8rSoAohs1yPJRz7Pa25/fi/blogreader",
@@ -111,7 +112,8 @@ export default {
         "/fi/sAYQdD47d8pXZu7Pzuas/fi/blogreader",
         "/fi/3BpLCRz0BsxDc7iVbUGW/fi/blogreader",
         "/fi/2GkIUneDfOvmL7wdHDnt/fi/blogreader",
-        "/fi/YQdLknRY696s8vMxCei4/fi/blogreader"
+        "/fi/YQdLknRY696s8vMxCei4/fi/blogreader",
+        "/fi/yigFvNFCf1Fd6qfGFKew/fi/blogreader"
       ]
     }]
   ],
